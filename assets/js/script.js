4ac8f3f363779d982b17943c38ff3cd076f90b1e
priyanka joshi <priyanka@sevenbits.in>
2022-06-02 23:32:13 +0530

    add all files

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel({
+//         loop:true,
+//         margin:10,
+//         nav:true,
+//         responsive:{
+//             0:{
+//                 items:1
+//             },
+//             600:{
+//                 items:3
+//             },
+//             1000:{
+//                 items:5
+//             }
+//         }
+//       });
+//   });
+// $(document).ready(function(){
+//     $('.one-time').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         adaptiveHeight: true
+//       });
+//   });
